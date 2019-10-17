8b9e7e423ec54e4e3eecb917b795b757c604b206
JustMonk <night.fire@mail.ru>
2019-10-17 23:03:47 +0500

    cls

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.storage.local.get('signed_in', function(data) {
-   if (data.signed_in) {
-     chrome.browserAction.setPopup({popup: 'popup.html'});
-   } else {
-     chrome.browserAction.setPopup({popup: 'popup_sign_in.html'});
-   }
- });
