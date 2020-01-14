c9da3e24a4d9b6113f543862ee290d015c8823d5
Осипов Анатолий Олегович <night.fire@mail.ru>
2020-01-14 07:52:24 +0500

    badge color

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
    if (request.type == "queueBadge") {
       if (request.options.queue > 0) {
+         let red = [250, 0, 0, 230];
+         let orange = [196, 128, 0, 230];
+         let green = [66, 137, 39, 230];
          //отображаем бадж с очередью
-         chrome.browserAction.setBadgeBackgroundColor({ color: [250, 0, 0, 230] });
+         if (request.options.queue <= 3) {
+            chrome.browserAction.setBadgeBackgroundColor({ color: red });
+         } else if (request.options.queue > 3 && request.options.queue <= 6) {
+            chrome.browserAction.setBadgeBackgroundColor({ color: orange });
+         } else {
+            chrome.browserAction.setBadgeBackgroundColor({ color: green });
+         }
          chrome.browserAction.setBadgeText({ text: `${request.options.queue}` });
       } else {
          //скрываем если нет необходимости
